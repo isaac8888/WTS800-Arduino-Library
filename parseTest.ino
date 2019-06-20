cea41d517eaa57cf0eaf576e738ab1e05802712d
Isaaac Rosenthal <isaac.rosenthal@duke.edu>
2019-06-20 15:21:48 -0600

    Gettingcloser

--- parseTest.ino
+++ parseTest.ino
@@ -37,7 +37,48 @@ void printWTSvalues(void)
   
   Serial.print("Dm: ");
   Serial.println(weather_packet.Dm);
+
+  Serial.print("Dx: ");
+  Serial.println(weather_packet.Dx);
+  
+  Serial.print("Sn: ");
+  Serial.println(weather_packet.Sn);
+  
+  Serial.print("Sm: ");
+  Serial.println(weather_packet.Sm);
+
+  Serial.print("Sx: ");
+  Serial.println(weather_packet.Sx);
+
+  Serial.print("Ta: ");
+  Serial.println(weather_packet.Ta);
+  
+  Serial.print("Ua: ");
+  Serial.println(weather_packet.Ua);
+
+  Serial.print("Pa: ");
+  Serial.println(weather_packet.Pa);
+
+  Serial.print("Rc: ");
+  Serial.println(weather_packet.Rc);
+  
+  Serial.print("Sr: ");
+  Serial.println(weather_packet.Sr);
+
+  Serial.print("Lux: ");
+  Serial.println(weather_packet.Lux);
+
+  Serial.print("T: ");
+  Serial.println(weather_packet.T);
+  
+  Serial.print("La: ");
+  Serial.println(weather_packet.La);
+
+  Serial.print("Lo: ");
+  Serial.println(weather_packet.Lo);
+
 }
+
 
 void parseTest(void)
 {
@@ -47,15 +88,19 @@ void parseTest(void)
     // Send R0 command and dump response into str
     Serial1.print("0R0\r\n");
     Serial1.readBytesUntil("\n", str, 512);
+
+    Serial.println(str);
     
-    sscanf(str, const char * format, 
-      weather_packet.Dn, weather_packet.Dm, weather_packet.Dx, 
-      weather_packet.Sn, weather_packet.Sm, weather_packet.Sx, 
-      weather_packet.Ta, weather_packet.Ua, weather_packet.Pa, 
-      weather_packet.Rc, weather_packet.Sr, weather_packet.Lux,
-      weather_packet.T, weather_packet.La, weather_packet.Lo);
-      
-  printWTSvalues();
+
+    sscanf(str,"0R0,Dn=%dD,Dm=%dD,Dx=%dD,Sn=%fM,Sm=%fM,Sx=%fM,Ta=%fC,Ua=%fP,Pa=%fH,Rc=%fM,Sr=%fW,Lux=%dL,T=%d,La=%d,N,Lo=%d,E", 
+      &weather_packet.Dn, &weather_packet.Dm, &weather_packet.Dx, 
+      &weather_packet.Sn, &weather_packet.Sm, &weather_packet.Sx, 
+      &weather_packet.Ta, &weather_packet.Ua, &weather_packet.Pa, 
+      &weather_packet.Rc, &weather_packet.Sr, &weather_packet.Lux,
+      &weather_packet.T, &weather_packet.La, &weather_packet.Lo);
+
+printWTSvalues();
+
 }
 
 /*********************************************************
@@ -76,4 +121,5 @@ void setup()
 
 void loop()
 {
+
 }
