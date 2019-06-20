7c7fca36eafb2f4e5d11315af7edfad94b256b4d
Isaaac Rosenthal <isaac.rosenthal@duke.edu>
2019-06-20 13:30:07 -0600

    update8

--- parseTest.ino
+++ parseTest.ino
@@ -44,44 +44,8 @@ void parseTest(void)
     {
       Serial.println(pch);
 
-      struct WTS weather_data;
-      struct WTS *sp;
-      weather_data.Dn = 4;
-      weather_data.wd = "Dn";
-      sp = &weather_data;
-      Serial.println(weather_data.wd);
 
 
-      //char str[512];
-      //scanf("%4d", weather);
-      //if (a != NULL)
-      //Serial.println(weather.weather_data);
-
-
-
-      //if there's a decimal point, use string to float
-
-    }
-
-
-
-    //string to long (put the result into the structure up top)
-
-
-
-    //if decimal point use string to float
-    //else if (decimal point)
-    //{
-    // string to double (put the result into the structure up top)
-    //char *ptr;
-    //double ret;
-    //if (ret != NULL)
-
-    //ret = strtod(str, &ptr);
-    //Serial.println(pch);
-    //}
-
-    pch = strtok (NULL, " ,");
   }
 
 }
